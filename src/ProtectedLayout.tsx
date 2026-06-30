6fabbf822c2664b8d1221b85c9b7cc88f123160d
karenbrite <karen.nicole.b1@gmail.com>
2026-06-30 00:16:33 -0300

    feat: complete header layout and basic dashboard structure

--- src/ProtectedLayout.tsx
+++ src/ProtectedLayout.tsx
@@ -2,14 +2,23 @@ import { Outlet } from "react-router";
 import { useConvexAuth, useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
 import { Teacher } from "../convex/teachers/types";
+import { Header } from "./components/UI/Header";
+
+
 
 export function ProtectedLayout() {
   const { isLoading, isAuthenticated } = useConvexAuth();
   const teacher = useQuery(api.teachers.queries.getCurrentTeacher);
 
-  console.log(teacher?._id)
   if (isLoading) return <p>Loading...</p>;
   if (!isAuthenticated || !teacher) return <div>Error</div>;
 
-  return <Outlet context={teacher satisfies Teacher} />;
+  return (
+    <div className="min-h-screen bg-[#F8FAFC]">
+      <Header />
+      <main>
+        <Outlet context={teacher satisfies Teacher} />
+      </main>
+    </div>
+  );
 }
