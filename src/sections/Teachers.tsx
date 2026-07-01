be38e4816e1ecf0497be7f10ddd49edb8f16ede3
karenbrite <karen.nicole.b1@gmail.com>
2026-07-01 00:41:01 -0300

    add sections with routing

---
+++ src/sections/Teachers.tsx
@@ -0,0 +1,13 @@
+export default function Teachers() {
+    
+  
+  return (
+    <div className="min-h-screen w-full bg-[#C6E5D9] flex flex-col pt-8 px-6">
+        <h2 className="font-angkor text-[40px] text-[#1E293B] font-normal mb-2 text-right">LISTA</h2>
+        <h3 className="text-4xl font-bold text-orange-500 mb-8 drop-shadow-sm text-right">Señoritas</h3>
+        
+
+    </div>
+  );
+
+  }
